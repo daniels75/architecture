72fe22299ed5f782b84003377ba0db753c27906c
daniels75 <daniel.sadowski75@gmail.com>
2020-01-16 19:26:00 +0100

    Simple Executor - part2

--- java-concurrency/src/main/java/com/javasolutions/concurrency/ExecutorExample.java
+++ java-concurrency/src/main/java/com/javasolutions/concurrency/ExecutorExample.java
@@ -5,7 +5,13 @@ import java.util.concurrent.Executor;
 public class ExecutorExample {
     public static void main(String[] args) {
         Executor executor = new SimpleExecutor();
-        System.out.println("CurrentTread: " + Thread.currentThread());
+        System.out.println("Current thread name: " + Thread.currentThread().getName());
+
+        executor.execute(() -> msg("Executed from executor in the same thread"));
+        System.out.println("-----------------------------------------------------------------");
+        Executor taskExecutor1 = new ThreadPerTaskExecutor();
+        taskExecutor1.execute(()->msg("Task executed in the another thread than the caller's thread."));
+
     }
 
     private static class SimpleExecutor implements Executor {
@@ -14,5 +20,17 @@ public class ExecutorExample {
         public void execute(Runnable command) {
             command.run();
         }
+    }
+
+    private static class ThreadPerTaskExecutor implements Executor {
+        public void execute(Runnable command) {
+            final Thread taskExecutor = new Thread(command);
+            msg("ThreadPerTaskExecutor, thread name: " + taskExecutor.getName());
+            taskExecutor.start();
+        }
+    }
+
+    private static void msg(String message) {
+        System.out.println(message);
     }
 }
