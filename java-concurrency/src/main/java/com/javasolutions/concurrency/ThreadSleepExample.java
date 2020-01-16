e10f7f9715da4903fd2a338398c275526943f5a7
daniels75 <daniel.sadowski75@gmail.com>
2020-01-16 20:34:37 +0100

    Added Runnable with sleep example

---
+++ java-concurrency/src/main/java/com/javasolutions/concurrency/ThreadSleepExample.java
@@ -0,0 +1,35 @@
+package com.javasolutions.concurrency;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class ThreadSleepExample {
+    public static void main(String[] args) throws Exception {
+        msg("Current thread: " + Thread.currentThread().getName());
+
+        final List<String> messages = Lists.newArrayList("message1", "message2", "message3");
+        final Thread thread = new Thread(createRunnable(messages));
+        thread.start();
+
+        Thread.sleep(500);
+        msg("Message from current thread: " + Thread.currentThread().getName());
+    }
+
+    private static Runnable createRunnable(List<String> messages) {
+        return () -> {
+                for(String message : messages) {
+                    msg(message);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new IllegalStateException(e);
+                    }
+                }
+            };
+    }
+
+    private static void msg(String message) {
+        System.out.println(message);
+    }
+}
