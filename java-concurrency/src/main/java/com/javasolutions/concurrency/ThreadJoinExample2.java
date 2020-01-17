e2510e325e6c182ee7c3c49172f30ccecf6bd82d
daniels75 <daniel.sadowski75@gmail.com>
2020-01-17 20:34:49 +0100

    Added another example of the join

---
+++ java-concurrency/src/main/java/com/javasolutions/concurrency/ThreadJoinExample2.java
@@ -0,0 +1,39 @@
+package com.javasolutions.concurrency;
+
+import java.time.LocalDateTime;
+
+public class ThreadJoinExample2 extends Thread {
+    private int processingCount;
+
+    ThreadJoinExample2(int processingCount) {
+        this.processingCount = processingCount;
+        msg("Thread created");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread simpleThread = new ThreadJoinExample2(2);
+        simpleThread.start();
+        msg("Invoking join");
+        // main thread is "waiting forever" for simpleThread
+        simpleThread.join();
+        msg("Returned from join");
+    }
+
+    private static void msg(String message) {
+        System.out.println(LocalDateTime.now() + " " + message);
+    }
+
+    @Override
+    public void run() {
+        msg("Thread " + this.getName() + " started");
+        while (processingCount > 0) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                msg("Thread " + this.getName() + " interrupted");
+            }
+            processingCount--;
+        }
+        msg("Thread " + this.getName() + " ended");
+    }
+}
