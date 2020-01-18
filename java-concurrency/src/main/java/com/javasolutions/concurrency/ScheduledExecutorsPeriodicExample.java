61a93836fb79a723d8a2b29ce3c653be1eeb3703
daniels75 <daniel.sadowski75@gmail.com>
2020-01-18 21:01:45 +0100

    Added Scheduler executed periodically

---
+++ java-concurrency/src/main/java/com/javasolutions/concurrency/ScheduledExecutorsPeriodicExample.java
@@ -0,0 +1,22 @@
+package com.javasolutions.concurrency;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorsPeriodicExample {
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+        Runnable task = () -> msg("Executing, time: " + LocalDateTime.now());
+
+
+        System.out.println("scheduling task to be executed every 3 seconds with an initial delay of 0 seconds");
+        scheduledExecutorService.scheduleAtFixedRate(task, 0,3, TimeUnit.SECONDS);
+    }
+
+    private static void msg(String message) {
+        System.out.println(message);
+    }
+}
